472cc2b7279a8c7e41e044e3a180d89413521277
Eban Sayago <eban.sayago@protonmail.com>
2022-09-06 22:23:55 -0300

    :sparkles: Document creation

--- src/components/Editor.ts
+++ src/components/Editor.ts
@@ -1,14 +1,16 @@
 import { css, html, LitElement } from 'lit';
-import { customElement, query } from 'lit/decorators.js'
+import { customElement, property, query } from 'lit/decorators.js'
+import { connect } from 'pwa-helpers';
 
 // Styles
 import { normalize } from '../styles/normalize';
 import { skeleton } from '../styles/skeleton';
 
 import { store } from '../redux/store';
+import { Doc, State } from '../redux/reducer';
 
 @customElement('md-editor')
-export class Editor extends LitElement {
+export class Editor extends connect(store)(LitElement) {
     static styles = [
         normalize,
         skeleton,
@@ -27,20 +29,37 @@ export class Editor extends LitElement {
                 justify-content: flex-start;
                 align-items: flex-start
             }
+
+            #writer {
+                color: white;
+            }
         `
     ]
 
     @query('textarea')
     textEl!: HTMLTextAreaElement
 
+    @property()
+    disabled = true
+
+    @property()
+    doc: Doc | null = null
+    collection: number = 0
+
     render() {
         return html`
         <div class="container u-full-width">
             <div id="actions" class="row">
+                <button class="button-primary" @click=${this.create}>Create document</button>
                 <button @click=${this.erase}>Erase</button>
             </div>
-            <div class="row">
-                <textarea @input=${this.input} class="u-full-width"></textarea>
+            <div id="writer" class="row">
+                <h1>${this.doc?.name}</h1>
+                <textarea
+                ?disabled=${this.disabled}
+                @input=${this.input}
+                class="u-full-width">
+                </textarea>
             </div>
         </div>
         `
@@ -50,10 +69,40 @@ export class Editor extends LitElement {
         this.textEl.value = ''
     }
 
+    create() {
+        store.dispatch({
+            type: 'ADD_DOC',
+            payload: `Untitled${this.collection}`,
+        })
+    }
+
     input() {
         store.dispatch({
             type: 'INPUT',
             payload: this.textEl.value
         })
+    }
+
+    firstUpdated() {
+        const state = store.getState()
+        console.log(state.doc)
+        
+        if (state.doc) {
+            this.doc = state.doc
+            this.textEl.value = state.doc.content
+            this.disabled = false
+        }
+    }
+
+    stateChanged(state: State): void {
+        this.collection = state.collection.length
+
+        if (state.doc) {
+            if (state.doc?.name !== this.doc?.name) {
+                this.doc = state.doc
+                this.textEl.value = state.doc.content
+                this.disabled = false
+            }
+        }
     }
 }
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,26 +1,85 @@
 import { Reducer } from "redux"
 
 export interface State {
-    text?: string,
-    count: number,
+    doc: {
+        name: string,
+        content: string,
+    } | null,
+    collection: Doc[],
 }
 
 export interface Action {
-    type: 'INPUT',
+    type: 'INPUT' | 'SET_DOC' | 'ADD_DOC',
     payload?: string
 }
 
-const initialState = {
-    text: 'Ping',
-    count: 0
+export interface Doc {
+    name: string,
+    content: string,
 }
 
-export const reducer: Reducer<State, Action> = (state = initialState, action) => {
+function getInitialDocument () {
+    let collection = JSON.parse(localStorage.getItem('collection') || 'null')
+
+    if (collection === null) {
+        localStorage.setItem('collection', JSON.stringify([]))
+        collection = JSON.parse(localStorage.getItem('collection') || 'null')
+    }
+
+    if (Array.isArray(collection)) {
+        if (collection.length > 0) {
+            return collection[0]
+        } else {
+            return null
+        }
+    } else {
+        console.error(`This shouldn't happen`)
+        return null
+    }
+}
+
+const initialState = {
+    doc: getInitialDocument(),
+    collection: JSON.parse(localStorage.getItem('collection') as string) // Can't be undefined
+}
+
+export const reducer: Reducer<State, Action> = (state = initialState, action): State => {
     switch (action.type) {
         case 'INPUT':
+            if (typeof action.payload !== "string") return state
             return {
                 ...state,
-                text: action.payload
+            }
+        case 'SET_DOC':
+            if (typeof action.payload !== "number") return state
+            return {
+                ...state,
+                doc: state.collection[action.payload] || state.collection[0]
+            }
+        case 'ADD_DOC':
+            if (typeof action.payload !== "string") return state
+
+            const filtered = state.collection.filter(doc => doc.name === action.payload)
+
+            if (filtered.length > 0) {
+                console.error(`Document "${action.payload}" already exists.`)
+                return state                
+            }
+
+            const newColl = [
+                ...state.collection,
+                {
+                    name: action.payload,
+                    content: ''
+                }
+            ]
+
+            localStorage.setItem('collection', JSON.stringify(newColl))
+
+            return {
+                ...state,
+                collection: newColl,
+                doc: newColl[newColl.length - 1]
             }
         default:
             return state
