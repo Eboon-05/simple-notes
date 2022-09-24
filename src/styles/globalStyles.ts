6dab3b06dcab502a52d688161f70a643b0640120
Eban Sayago <eban.sayago@protonmail.com>
2022-09-24 16:12:42 -0300

    :rotating_light: Wasn't building because of a type error

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -6,7 +6,7 @@ function getDocumentStyles () {
     for (let i = 0; i < document.styleSheets.length; i++) {
         const { cssRules } = document.styleSheets[i]
         
-        stylesArray.push(css([Object.values(cssRules).map(rule => rule.cssText).join('\n')]))
+        stylesArray.push(css([Object.values(cssRules).map(rule => rule.cssText).join('\n')] as unknown as TemplateStringsArray))
     }
     
     return stylesArray
