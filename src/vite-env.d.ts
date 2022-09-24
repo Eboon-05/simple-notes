04e47a4f153602ce6b9561fd1ea532946892655b
Eban Sayago <eban.sayago@protonmail.com>
2022-09-24 20:01:07 -0300

    :label: Added travelerjs types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+declare module 'travelerjs' {
+    export class Traveler {
+        register(route: Route)
+        listen()
+        go(route: string)
+    }
+
+    export class Route {
+        constructor(path: string, callback: (params: string) => void)
+    }
+}
